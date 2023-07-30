30233eb0a6f075918dc37f607ea9117f4ce32d7a
priyanshu <priyanshumaurya2810@gmail.com>
2023-07-30 18:10:13 +0530

    using chessboard.js library , done with displaying chess board

---
+++ main.js
@@ -0,0 +1,12 @@
+var config = {
+  draggable: true,
+  position: "start",
+  pieceTheme: "img/{piece}.png",
+  //   onDragStart: onDragStart,
+  //   onDrop: onDrop,
+  //   onMouseoutSquare: onMouseoutSquare,
+  //   onMouseoverSquare: onMouseoverSquare,
+  //   onSnapEnd: onSnapEnd,
+};
+
+var board = Chessboard("myBoard", config);
