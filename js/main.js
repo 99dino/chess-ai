e5be033664669fbe29b4e382de5c57fc3b4e6d4f
priyanshu <priyanshumaurya2810@gmail.com>
2023-07-31 13:33:21 +0530

    restructuring contents

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ var config = {
   draggable: true,
   position: "start",
   pieceTheme: "img/{piece}.png",
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onMouseoutSquare: onMouseoutSquare,
-  onMouseoverSquare: onMouseoverSquare,
-  onSnapEnd: onSnapEnd,
+  // onDragStart: onDragStart,
+  // onDrop: onDrop,
+  // onMouseoutSquare: onMouseoutSquare,
+  // onMouseoverSquare: onMouseoverSquare,
+  // onSnapEnd: onSnapEnd,
 };
 
 var board = Chessboard("myBoard", config);
