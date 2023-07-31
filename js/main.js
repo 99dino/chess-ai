2d33a317df73d4add6e19189f4892407dfb2fcd7
priyanshu <priyanshumaurya2810@gmail.com>
2023-07-31 22:02:41 +0530

    event listener - loading ruy lopez game opening

--- js/main.js
+++ js/main.js
@@ -480,3 +480,17 @@ function reset() {
     timer = null;
   }
 }
+
+/*
+ * Event listeners for various buttons.
+ */
+$("#ruyLopezBtn").on("click", function () {
+  reset();
+  game.load(
+    "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 1"
+  );
+  board.position(game.fen());
+  window.setTimeout(function () {
+    makeBestMove("b");
+  }, 250);
+});
