f4f93aee21d4ce4d4ba14b113258439c74106ec0
priyanshu <priyanshumaurya2810@gmail.com>
2023-07-31 22:08:23 +0530

    highlighting and allowing only legal moves - adapted from chessboard.js examples #5000

--- js/main.js
+++ js/main.js
@@ -581,3 +581,99 @@ document.addEventListener("keydown", (event) => {
     document.getElementById("redoBtn").click();
   }
 });
+
+/*
+ * The remaining code is adapted from chessboard.js examples #5000 through #5005:
+ * https://chessboardjs.com/examples#5000
+ */
+function removeGreySquares() {
+  $("#myBoard .square-55d63").css("background", "");
+}
+
+function greySquare(square) {
+  var $square = $("#myBoard .square-" + square);
+
+  var background = whiteSquareGrey;
+  if ($square.hasClass("black-3c85d")) {
+    background = blackSquareGrey;
+  }
+
+  $square.css("background", background);
+}
+
+function onDragStart(source, piece) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false;
+
+  // or if it's not that side's turn
+  if (
+    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+    (game.turn() === "b" && piece.search(/^w/) !== -1)
+  ) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  undo_stack = [];
+  removeGreySquares();
+
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: "q", // NOTE: always promote to a queen for example simplicity
+  });
+
+  // Illegal move
+  if (move === null) return "snapback";
+
+  globalSum = evaluateBoard(game, move, globalSum, "b");
+  updateAdvantage();
+
+  // Highlight latest move
+  $board.find("." + squareClass).removeClass("highlight-white");
+
+  $board.find(".square-" + move.from).addClass("highlight-white");
+  squareToHighlight = move.to;
+  colorToHighlight = "white";
+
+  $board
+    .find(".square-" + squareToHighlight)
+    .addClass("highlight-" + colorToHighlight);
+
+  if (!checkStatus("black"));
+  {
+    // Make the best move for black
+    window.setTimeout(function () {
+      makeBestMove("b");
+    }, 250);
+  }
+}
+
+function onMouseoverSquare(square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true,
+  });
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  // highlight the square they moused over
+  greySquare(square);
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to);
+  }
+}
+
+function onMouseoutSquare(square, piece) {
+  removeGreySquares();
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
